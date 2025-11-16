cf0eea0e1b7723dfe81f6f42e94cd253e7edf20a
njrgitman-jr <njr.gitman@gmail.com>
2025-11-16 12:47:29 +0300

    try to redeploy

--- server/route/dashboard.route.js
+++ server/route/dashboard.route.js
@@ -6,17 +6,6 @@ import { admin } from "../middleware/admin.js";
 const dashboardRouter = express.Router();
 
 // Base URL: /api/dashboard
-dashboardRouter.get("/overview", auth, async (req, res) => {
-  try {
-    await dashboardOverview(req, res);
-  } catch (error) {
-    console.error("Error in /overview route:", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-      error: error.message,
-    });
-  }
-});
+dashboardRouter.get("/overview", auth, admin, dashboardOverview);
 
 export default dashboardRouter;
