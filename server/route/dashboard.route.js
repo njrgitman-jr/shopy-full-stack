5a8f3bbd22baef0d5cc1fa8168bd314aa9c318ec
njrgitman-jr <njr.gitman@gmail.com>
2025-11-16 12:41:47 +0300

    added try cach to admin.js

--- server/route/dashboard.route.js
+++ server/route/dashboard.route.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { dashboardOverview } from "../controllers/dashboard.controller.js";
 import auth from "../middleware/auth.js";
-
 import { admin } from "../middleware/admin.js";
 
 const dashboardRouter = express.Router();
 
 // Base URL: /api/dashboard
-dashboardRouter.get("/overview", auth, admin, dashboardOverview);
+dashboardRouter.get("/overview", auth, admin, async (req, res) => {
+  try {
+    await dashboardOverview(req, res);
+  } catch (error) {
+    console.error("Error in /overview route:", error);
+    res.status(500).json({ 
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+});
 
 export default dashboardRouter;
